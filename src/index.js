cd86b9d58d83d3140b767fe47256483e69b85a2b
Fer Hernandez <fh.1111@gmail.com>
2019-03-01 02:15:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ReactDOM from 'react-dom'
 // import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 // import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
